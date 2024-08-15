32511e5ba6489fb8d9c9b7e80e9b47ec37a24129
Maximilian Ygdell <maxygdell@protonmail.com>
2024-08-15 09:20:33 +0200

    Added exercises for reading code, understanding bugs.

---
+++ exercises/code/ArrayIndex.java
@@ -0,0 +1,16 @@
+/* 
+ * Running this code will produce an error.
+ * Your task is to identify the issue,
+ * understand why it happens and fix it.
+ */
+
+public class ArrayIndex {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int number = 0;
+
+        for (int i = 0; i <= numbers.length; i++) {
+            number = numbers[i];
+        }
+    }
+}
---
+++ exercises/code/StringComparison.java
@@ -0,0 +1,23 @@
+/* 
+* Explain why the results are different when calling 
+* isGreetingsHello with "the same" string. 
+ */
+
+ public class StringComparison {
+
+    public static void isGreetingsHello(String greeting) {
+        if (greeting == "Hello") {
+            System.out.println("Greeting is Hello");
+        } else {
+            System.out.println("Greeting is not Hello");
+        }
+
+    }
+    public static void main(String[] args) {
+        String greeting = "Hello";
+        String greetings = new String("Hello");
+
+        isGreetingsHello(greeting);
+        isGreetingsHello(greetings);
+    }
+}
